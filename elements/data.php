f76811790c7c2394cf5c2f55c18cf218cb7d7190
zych.johann@gmail.com <zych.johann@gmail.com>
2022-10-05 16:03:02 +0200

    Border buttons

--- elements/data.php
+++ elements/data.php
@@ -6,7 +6,7 @@ $dataFrench = [
     ["- Epéiste reconnu", "- Dirigeant, leadership, exemplarité", "- Engagé, résolu", "- Eloquence et négocations", "- Monter à cheval", "- Monter à dragon", "- Immortel"],
     ["- Apprentissage du maniement de l'épée et de l'arc, en tant que Gardien du Mur et Bâtard de Winterfell", "- Rudiments de la Politique et des négociations", "- Eloquence et argumentation", "- Chevauche de chevaux et de dragons", "- Apprivoisement des bêtes", "- Education de loups"],
     ["Je suis un leader efficace, qui a le pouvoir de motiver des personnes fidèles à atteindre et à dépasser leur potentiel. Je prêche par l'exemple et j'ai un flair naturel pour le leadership avec un palmarès éprouvé de victoires."],
-    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 rue du pied du Mur", "99 999 Châteaunoir", "WESTEROS", "<h3>Disponible</h3>"],
+    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 rue du pied du Mur", "99 999 Châteaunoir", "WESTEROS", "<strong>Disponible</strong>"],
     ["Choisis une langue"],
     ["Lord Commandant de la Garde de Nuit"],
 ];
@@ -17,7 +17,7 @@ $dataEnglish = [
     ["- Reputed swordsman", "- Leader, leadership, exemplary nature", "- Engaged, determined", "- Eloquent and negociations", "- Horse Riding", "- Dragon riding", "- Immortal"],
     ["- Training in sword and bow manipulation, as knight of the Night's Watch et Bastard of Winterfell", "- Politics and negociations rudiments ", "- Eloquence and argumentations", "- Horse and dragon riding", "- Taming a beast", "- Wolf education"],
     ["I am an effective leader, that has the power to motivate loyal people to reach and exceed their potential. I lead by example and I have a natural flair for leadership with a proven track record of winning."],
-    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 road foot of the wall", "99 999 Castleblack", "WESTEROS", "<h3>Available</h3>"],
+    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 road foot of the wall", "99 999 Castleblack", "WESTEROS", "<strong>Available</strong>"],
     ["Select a language"],
     ["Lord Commander of the Night's Watch"],
 ];
@@ -28,7 +28,7 @@ $dataValyrian = [
     ["- Nyke ȳdra daor gīmigon skoros naejot", "- Bardugon yn nyke", "- Nyke sepār jorrāelagon", "- Iksis jāre naejot shifang skoros", "- Nyke jorrāelagon tolī ra naejot dīnagon"],
     ["- Bisa ȳdrassis va Ēlīedar lantpyro bȳre ampā sētīles", "- Se ēza issare mazverdāre mirre pār", "- Mirre udra māzigon hen Valyrio Eglie", "- Valyriā Mīrīnot tembyrōva va", "- Lo ao jorrāelagon iā qantre", "- Idrir iā brōzi arliñagon"],
     ["Skoriot ñuhyz zaldrīzesse ilzi, nuhor līr gūrēnna. Sīkudi nopāzmi! Daoruni gīmī, Ionos Sōnaro. Skorī dēmalȳti tymptir tymis, ērinis iā morghūlis. Bantis zōbrie issa se ossȳngnoti lēdys."],
-    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 geralbar kaist hen dōros", "99 999 Castleblack", "WESTEROS", "<h3>Jorrāelagon</h3>"],
+    ["jon.snow@knowsnothing.com", "01.05.02.05.02", "4 geralbar kaist hen dōros", "99 999 Castleblack", "WESTEROS", "<strong>Jorrāelagon</strong>"],
     ["Iderēbagon nykeā udrir"],
     ["Āeksio jentys hen Urnēbagon Bantior"],
 
